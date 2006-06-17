71f641bdb52c6e9c31783cc00a4cb2efd625510d
W. John Braun <braun@stats.uwo.ca>
2006-06-17 00:00:00 +0000

    version 0.84

--- R/bounce.R
+++ R/bounce.R
@@ -29,7 +29,7 @@ function(y, d, log=FALSE)
           for(k in i1:jn)
             xnew[k] <- x1 - jot + (k - i1) * d
           i1 <- jn + 1
-          nobounce <- F
+          nobounce <- FALSE
           break
         }
       }
@@ -39,7 +39,7 @@ function(y, d, log=FALSE)
     if(min(diff(xnew)) < d * 0.999) {
       n1 <- (1:(n - 1))[diff(xnew) < d]
       cat("Error in bounce().  Improperly separated points are:",
-          fill = T)
+          fill = TRUE)
       cat(paste(n1, ":", n1 + 1, sep = ""), fill = TRUE)
       cat(paste(xnew[n1], ":", xnew[n1 + 1], sep = ""), fill
           = TRUE)
--- R/datafile.R
+++ R/datafile.R
@@ -5,28 +5,32 @@ function (file = "fuel")
         cat("year  carbon", "1800   8", "1850   54", "1900   534", 
             "1950   1630", "2000   6611", file = "fuel.txt", 
             sep = "\n")
-    print(paste("File fuel.txt is now in directory", getwd()))
+        print(paste("File fuel.txt is now in directory", getwd()))
     }
     if (file == "fuel.csv") {
-        cat("year,carbon", "1800,8", "1850,54", "1900,534",
-            "1950,1630", "2000,6611", file = "fuel.csv",
-            sep = "\n")
-   print(paste("File fuel.csv is now in directory", getwd()))
- 
+        cat("year,carbon", "1800,8", "1850,54", "1900,534", "1950,1630", 
+            "2000,6611", file = "fuel.csv", sep = "\n")
+        print(paste("File fuel.csv is now in directory", getwd()))
     }
     if (file == "oneBadRow") {
         cat("10 9 17  # First of 7 lines", "11 13 1 6", "9 14 16", 
             "12 15 14", "8 15 15", "9 13 12", "7 14 18", file = "oneBadRow.txt", 
             sep = "\n")
-   print(paste("File oneBadRow.txt is now in directory", getwd()))
- 
-   }
+        print(paste("File oneBadRow.txt is now in directory", 
+            getwd()))
+    }
     if (file == "scan-demo") {
         cat("First of 4 lines", "a 2 3", "b 11 13", "c 9 7", 
             file = "scan-demo.txt", sep = "\n")
-   print(paste("File scan-demo.txt is now in directory", getwd()))
-  
-   }
-invisible()
+        print(paste("File scan-demo.txt is now in directory", 
+            getwd()))
+    }
+    if (file == "bostonc") {
+         cat(bostonc[1:9], file="bostonc.txt", sep="\t", fill=TRUE) 
+         cat("\n", file="bostonc.txt", sep="\t", append=TRUE) 
+         cat(bostonc[-c(1:9)], file="bostonc.txt", sep="\t",fill=TRUE,append=TRUE) 
+        print(paste("File bostonc.txt is now in directory", getwd()))
+    }
+    invisible()
 }
 
