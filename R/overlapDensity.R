66d2470917ce8d697b2090741f4d6eca58e1d0bd
W. John Braun <braun@stats.uwo.ca>
2006-12-13 00:00:00 +0000

    version 0.91

--- R/overlapDensity.R
+++ R/overlapDensity.R
@@ -1,27 +1,26 @@
-"overlapDensity" <-
+`overlapDensity` <-
 function (x0, x1, ratio = c(0.05, 20), compare.numbers = FALSE, 
-            plotit = TRUE, gpnames = c("Control", "Treatment"),
-            cutoffs=c(lower=TRUE, upper=TRUE), bw=FALSE,
-            xlab = "Score", col=1:2, lty=1:2) 
+            plotit = TRUE, gpnames = c("Control", "Treatment"), cutoffs = c(lower = TRUE, 
+                                                                  upper = TRUE), bw = FALSE, xlab = "Score", col = 1:2, 
+            lty = 1:2) 
 {
   ran <- range(c(x0, x1))
-  if(all(cutoffs)){
-    d0 <- density(x0, from=ran[1], to=ran[2])
-    d1 <- density(x1, from=ran[1], to=ran[2])
+  if (all(cutoffs)) {
+    d0 <- density(x0, from = ran[1], to = ran[2])
+    d1 <- density(x1, from = ran[1], to = ran[2])
   }
-  else if (cutoffs[1]){
-    d0 <- density(x0, from=ran[1])
-    d1 <- density(x1, from=ran[1])
+  else if (cutoffs[1]) {
+    d0 <- density(x0, from = ran[1])
+    d1 <- density(x1, from = ran[1])
   }
-  else if (cutoffs[2]){
-    d0 <- density(x0, from=ran[2])
-    d1 <- density(x1, from=ran[2])
+  else if (cutoffs[2]) {
+    d0 <- density(x0, to = ran[2])
+    d1 <- density(x1, to = ran[2])
   }
   else {
     d0 <- density(x0)
     d1 <- density(x1)
   }
-  
   if (compare.numbers) {
     n0 <- length(x0)
     n1 <- length(x1)
@@ -38,18 +37,21 @@ function (x0, x1, ratio = c(0.05, 20), compare.numbers = FALSE,
   ylim <- range(c(0, f0, f1))
   ylim[2] <- ylim[2] + 0.1 * diff(ylim)
   if (plotit) {
-    plot(d1$x, f1, xlim = xlim, xlab = xlab, xaxt = "n", bty = "n",
-         yaxs = "i", ylim = ylim, ylab = ylab, main = "", type="n") 
+    oldpar <- par(no.readonly=TRUE)
+    on.exit(par(oldpar))
+    plot(d1$x, f1, xlim = xlim, xlab = xlab, xaxt = "n", 
+         bty = "n", yaxs = "i", ylim = ylim, ylab = ylab, 
+         main = "", type = "n")
     axis(1)
     box(bty = "L")
-    lines(d0$x, f0, col=col[1], lty = lty[1])
-    if(bw & lty[2]>1) lines(d1$x, f1, col = "gray40", lty = 1)
+    lines(d0$x, f0, col = col[1], lty = lty[1])
+    if (bw & lty[2] > 1) 
+      lines(d1$x, f1, col = "gray40", lty = 1)
     lines(d1$x, f1, col = col[2], lty = lty[2])
     xpos <- par()$usr[1]
     ypos <- par()$usr[4]
-    legend(xpos, ypos, lty = lty, col = col, 
-           cex = c(0.8, 0.8), legend = gpnames,
-           bty = "n")
+    legend(xpos, ypos, lty = lty, col = col, cex = 0.8,
+           legend = gpnames, bty = "n")
   }
   if (is.null(ratio)) 
     return()
@@ -77,12 +79,12 @@ function (x0, x1, ratio = c(0.05, 20), compare.numbers = FALSE,
     xlim[2] <- min(x01[hichoose])
   else xlim[2] <- max(x01)
   if (plotit) {
-    axis(3, at = xlim, labels = paste(signif(xlim, 4)), 
-         mgp=c(3,0.5,0), col="gray", line=1, cex.axis=0.8)
-    axis(3, at = xlim, line=1,
-         labels = c(paste("1:",round(1/ratio[1]),sep=""),
-           paste(round(ratio[2]),":1",sep="")),
-         mgp=c(-3,-1,0), col="gray", cex.axis=0.8, lty=0)    
+    axis(3, at = xlim, labels = paste(signif(xlim, 4)), mgp = c(3, 
+                                                          0.5, 0), col = "gray", line = 1, cex.axis = 0.8)
+    axis(3, at = xlim, line = 1, labels = c(paste("1:", round(1/ratio[1]), 
+                                   sep = ""), paste(round(ratio[2]), ":1", sep = "")), 
+         mgp = c(-3, -1, 0), col = "gray", cex.axis = 0.8, 
+         lty = 0)
   }
   xlim
 }
