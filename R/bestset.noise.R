6957fa785691680c23c82d1f8ae2aae340968e07
W. John Braun <braun@stats.uwo.ca>
2011-08-02 00:00:00 +0000

    version 1.08

--- R/bestset.noise.R
+++ R/bestset.noise.R
@@ -1,20 +1,22 @@
 bestset.noise <-
-function (m = 100, n = 40, method = "exhaustive", nvmax = 3, print.summary=TRUE) 
+function (m = 100, n = 40, method = "exhaustive", nvmax = 3,
+              print.summary = TRUE, really.big=FALSE)
 {
-  leaps.out <- try(require(leaps), silent = TRUE)
-  if ((is.logical(leaps.out) == TRUE) & (leaps.out == TRUE)) {
-    y <- rnorm(m)
-    xx <- matrix(rnorm(m * n), ncol = n)
-    dimnames(xx) <- list(NULL, paste("V", 1:n, sep = ""))
-    u <- regsubsets(xx, y, method = method, nvmax = nvmax, 
-                    nbest = 1)
-    x <- xx[, summary(u)$which[nvmax, -1]]
-    u1 <- lm(y ~ x)
-    if(print.summary) print(summary(u1, corr = FALSE))
-    invisible(u1)
-  }
-  else {
-    print("Error: package leaps is not installed properly")
-  }
+    leaps.out <- try(require(leaps), silent = TRUE)
+    if ((is.logical(leaps.out) == TRUE) & (leaps.out == TRUE)) {
+        y <- rnorm(m)
+        xx <- matrix(rnorm(m * n), ncol = n)
+        dimnames(xx) <- list(NULL, paste("V", 1:n, sep = ""))
+        u <- regsubsets(xx, y, method = method, nvmax = nvmax,
+                        nbest = 1, really.big=really.big)
+        x <- xx[, summary(u)$which[nvmax, -1]]
+        u1 <- lm(y ~ x)
+        if (print.summary)
+            print(summary(u1, corr = FALSE))
+        invisible(u1)
+    }
+    else {
+        print("Error: package leaps is not installed properly")
+    }
 }
 
--- R/bsnCV.R
+++ R/bsnCV.R
@@ -1,30 +1,29 @@
 bsnCV <-
-function (m = 100, n = 40, method = "exhaustive", nvmax = 3, nfolds=2,
-            print.summary=TRUE)
+function (m = 100, n = 40, method = "exhaustive", nvmax = 3,
+              nfolds = 2, print.summary = TRUE, really.big=FALSE)
 {
-  leaps.out <- try(require(leaps), silent = TRUE)
-  if (!is.logical(leaps.out) | (leaps.out == FALSE)) {
-    print("Error: package leaps is not installed properly")
-    return()
-  }
+    leaps.out <- try(require(leaps), silent = TRUE)
+    if (!is.logical(leaps.out) | (leaps.out == FALSE)) {
+        print("Error: package leaps is not installed properly")
+        return()
+    }
     y <- rnorm(m)
     xx <- matrix(rnorm(m * n), ncol = n)
     dimnames(xx) <- list(NULL, paste("V", 1:n, sep = ""))
-    foldid <- sample(1:nfolds, m, replace=TRUE)
-    objlist <- vector("list", length=nfolds)
-    for (i in 1:nfolds){
-      train <- foldid!=i
-      test <- !train
-      xxi <- xx[train,]
-      yi <- y[train]
-      u <- regsubsets(xxi, yi, method = method, nvmax = nvmax, 
-                    nbest = 1)
-      x <- xx[test, summary(u)$which[nvmax, -1]]
-      objlist[[i]] <- lm(y[test] ~ x)
+    foldid <- sample(1:nfolds, m, replace = TRUE)
+    objlist <- vector("list", length = nfolds)
+    for (i in 1:nfolds) {
+        train <- foldid != i
+        test <- !train
+        xxi <- xx[train, ]
+        yi <- y[train]
+        u <- regsubsets(xxi, yi, method = method, nvmax = nvmax,
+                        nbest = 1, really.big=really.big)
+        x <- xx[test, summary(u)$which[nvmax, -1]]
+        objlist[[i]] <- lm(y[test] ~ x)
     }
-  if(print.summary)
-    for(i in 1:nfolds)
-      print(summary(objlist[[i]]))
+    if (print.summary)
+        for (i in 1:nfolds) print(summary(objlist[[i]]))
     invisible(objlist)
 }
 
